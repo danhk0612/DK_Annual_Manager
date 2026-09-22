ba9f9ab528fe86b0e4818456b7903b23be0494a7
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-22 15:09:36 +0900

    Carry deep link into Telegram login

--- templates/login.php
+++ templates/login.php
@@ -1,5 +1,6 @@
 <?php
 /** @var bool $configured */
+/** @var string $returnTo */
 /** @var mixed $message */
 ?>
 <section class="panel narrow">
@@ -10,7 +11,7 @@
     <?php endif; ?>
     <?php if ($configured): ?>
         <p>Telegram 계정으로 로그인합니다. 최초 로그인 사용자는 관리자 승인 전까지 대기 상태로 등록됩니다.</p>
-        <a class="button primary" href="/auth/telegram/start">Telegram으로 계속</a>
+        <a class="button primary" href="/auth/telegram/start?return_to=<?= rawurlencode($returnTo) ?>">Telegram으로 계속</a>
     <?php else: ?>
         <p>`config/config.php`에 Telegram Client ID, Client Secret, Redirect URI를 설정해야 합니다.</p>
     <?php endif; ?>
